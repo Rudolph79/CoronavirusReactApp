7e24ff8f02a79e49bf5238076ee54bd6c295bc1c
EBANG MEZUI ME MVE <rudolph.ebang@gmail.com>
2020-11-03 11:44:17 +0100

    Updates

---
+++ precache-manifest.ced774b570b4f9d7c70134f9afa37585.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fca5239051a33bc1efd9d079ffe1818",
+    "url": "/CoronavirusReactApp/index.html"
+  },
+  {
+    "revision": "99b4aa1c25303adb820c",
+    "url": "/CoronavirusReactApp/static/css/main.6570e649.chunk.css"
+  },
+  {
+    "revision": "7b8cb79b7121f7625a66",
+    "url": "/CoronavirusReactApp/static/js/2.e30e28d5.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/CoronavirusReactApp/static/js/2.e30e28d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99b4aa1c25303adb820c",
+    "url": "/CoronavirusReactApp/static/js/main.b2975592.chunk.js"
+  },
+  {
+    "revision": "c878782704d440cac79d",
+    "url": "/CoronavirusReactApp/static/js/runtime-main.2ac29e32.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/CoronavirusReactApp/static/media/image.d7265326.png"
+  }
+]);
